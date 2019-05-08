261f6026eaa5d42bc1c9ce40dd0a6d97de396cee
ajay-van <40171808+ajay-van@users.noreply.github.com>
2019-05-08 15:49:52 +0530

    to pass
    Azure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import{DataService} from './htmldataservice';
 import{HtmlModel} from './model';
 import {QuestionControlService} from './dynamicform.controlservice';
 import{FormGroup} from'@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
